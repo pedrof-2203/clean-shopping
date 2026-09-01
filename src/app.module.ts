8c36917078ccee9a66e789c35c3583eab8cfb23e
pedro <pedroantoniofpa22@gmail.com>
2026-09-01 18:03:22 -0300

    feat: add DrizzleModule and DrizzleProvider for PostgreSQL integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from 'node_modules/@nestjs/config/dist/config.module';
 import { MongoModule } from './shared/infrastructure/database/mongodb/mongo.module';
+import { DrizzleModule } from './shared/infrastructure/database/postgres/drizzle.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), MongoModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongoModule,
+    DrizzleModule,
+  ],
 })
 export class AppModule {}
