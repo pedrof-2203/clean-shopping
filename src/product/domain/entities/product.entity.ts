3de691c9cf747a6f8e7071f02c7ebccdf6374413
pedro <pedroantoniofpa22@gmail.com>
2026-09-01 22:41:04 -0300

    fix: product validation bugs

--- src/product/domain/entities/product.entity.ts
+++ src/product/domain/entities/product.entity.ts
@@ -114,13 +114,13 @@ export class Product extends AggregateRoot {
   }
 
   private static validateName(name: string): void {
-    if (name.length > 2) {
+    if (name.length < 2) {
       throw new Error('Product name must be atleast 2 characters long.');
     }
   }
 
   private static validateStock(stock: number): void {
-    if (stock > 0) {
+    if (stock < 0) {
       throw new Error('Stock cannot be negative.');
     }
   }
